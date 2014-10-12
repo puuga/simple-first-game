ddaf4bd04e903974314d1aa7de90c19465ce11ab
puuga <siwaoh@gmail.com>
2014-10-12 11:21:57 +0700

    Initial Commit

---
+++ simple first gameTests/simple_first_gameTests.swift
@@ -0,0 +1,36 @@
+//
+//  simple_first_gameTests.swift
+//  simple first gameTests
+//
+//  Created by Siwawes Wongcharoen on 10/12/2557 BE.
+//  Copyright (c) 2557 puuga. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class simple_first_gameTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
