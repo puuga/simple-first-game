68926d7c1c5885116b8f65de0086688e12cf57ae
puuga <siwaoh@gmail.com>
2014-10-12 14:20:25 +0700

    update but not complete

--- simple first game/ViewController.swift
+++ simple first game/ViewController.swift
@@ -12,15 +12,32 @@ class ViewController: UIViewController {
     
     var lbCoundown:UILabel!;
     var btStart:UIButton!;
+    var lbQuestion:UILabel!;
+    var lbLive:UILabel!;
+    var btTrue:UIButton!;
+    var btFalse:UIButton!;
     
     var timer:NSTimer!;
     var time = 10;
+    var initLive = 5;
+    var currentLive:Int!;
+    
+    var questionList = [
+        ["Monday is yellow",true],
+        ["Jedi is normal guy",false]];
+    var currentQuestionIndex = 0;
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        
+        
         bindWidget();
+        
+        currentLive = initLive;
+        
+        nextQuestion();
     }
 
     override func didReceiveMemoryWarning() {
@@ -35,11 +52,34 @@ class ViewController: UIViewController {
         lbCoundown.text = "-";
         self.view.addSubview(lbCoundown);
         
-        btStart = UIButton(frame: CGRect(x: 40, y: 160, width: 300, height: 80));
-        btStart.setTitle("Play", forState: .Normal);
-        btStart.setTitleColor(UIColor.blueColor(), forState: .Normal);
-        btStart.addTarget(self, action: "start", forControlEvents: .TouchUpInside);
-        self.view.addSubview(btStart);
+        lbQuestion = UILabel(frame: CGRect(x: 40, y: 280, width: 300, height: 80));
+        lbQuestion.textAlignment = .Center;
+        lbQuestion.text = "-";
+        self.view.addSubview(lbQuestion);
+        
+        lbLive = UILabel(frame: CGRect(x: 40, y: 420, width: 300, height: 40));
+        lbLive.textAlignment = .Center;
+        lbLive.text = "-";
+        self.view.addSubview(lbLive);
+        
+        
+//        btStart = UIButton(frame: CGRect(x: 40, y: 160, width: 300, height: 40));
+//        btStart.setTitle("Play", forState: .Normal);
+//        btStart.setTitleColor(UIColor.blueColor(), forState: .Normal);
+//        btStart.addTarget(self, action: "start", forControlEvents: .TouchUpInside);
+//        self.view.addSubview(btStart);
+        
+        btTrue = UIButton(frame: CGRect(x: 40, y: 360, width: 80, height: 40));
+        btTrue.setTitle("True", forState: .Normal);
+        btTrue.setTitleColor(UIColor.blueColor(), forState: .Normal);
+        btTrue.addTarget(self, action: "answer:", forControlEvents: .TouchUpInside);
+        self.view.addSubview(btTrue);
+        
+        btFalse = UIButton(frame: CGRect(x: 160, y: 360, width: 80, height: 40));
+        btFalse.setTitle("False", forState: .Normal);
+        btFalse.setTitleColor(UIColor.blueColor(), forState: .Normal);
+        btFalse.addTarget(self, action: "answer:", forControlEvents: .TouchUpInside);
+        self.view.addSubview(btFalse);
         
     }
     
@@ -49,7 +89,7 @@ class ViewController: UIViewController {
         }
         timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: "timeFire", userInfo: nil, repeats: true)
         lbCoundown.text = "\(time)";
-        btStart.setTitle("Reset", forState: .Normal);
+        //btStart.setTitle("Reset", forState: .Normal);
         
     }
     
@@ -67,6 +107,36 @@ class ViewController: UIViewController {
         timer.invalidate();
         time = 10;
         btStart.setTitle("Play", forState: .Normal);
+    }
+    
+    func answer(sender:UIButton) {
+        if sender.titleLabel?.text == "True" {
+            checkAnswer(true);
+        } else {
+            checkAnswer(false);
+        }
+    }
+    
+    func checkAnswer(answer:Bool) {
+        if (Bool(questionList[currentQuestionIndex][0] as NSNumber)) {
+            currentLive = currentLive!+1;
+        } else {
+            currentLive = currentLive!-1;
+        }
+        updateLive();
+        nextQuestion();
+    }
+    
+    func updateLive() {
+        lbLive.text = "\(currentLive)"
+    }
+    
+    func nextQuestion() {
+        if currentQuestionIndex < questionList.count {
+            lbQuestion.text = String(questionList[currentQuestionIndex][0] as NSString);
+            currentQuestionIndex++;
+            start();
+        }
     }
 
 }
